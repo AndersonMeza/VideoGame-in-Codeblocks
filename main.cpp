72ad81db5c25d67be0bff0bc5b6c099e1ee128c1
Anderson Meza <mezacando.anderson@gmail.com>
2020-04-09 17:30:23 -0500

    Pongo paredes verdes

--- main.cpp
+++ main.cpp
@@ -14,6 +14,7 @@
 
 double ancho=1200, alto=700;
 double camarax=0,camaraz=0;
+double girocamarax = camarax, gorocamaraz=camaraz-10;
 
 
 
@@ -21,17 +22,17 @@ void piso()
 {
     glPushMatrix();
     glColor3d(1,1,1);
-    glTranslated(0,-1,-25);
+    glTranslated(0,0,-30);
     glRotated(0,0,0,0);
 
     glPushMatrix();
-    glScaled(50,1,50);
+    glScaled(50,0.2,50);
     glutSolidCube(1);
     glPopMatrix();
 
     glPushMatrix();
     glColor3d(0.8,0.8,0.8);
-    glScaled(50,1,50);
+    glScaled(50,0.2,50);
     glutWireCube(1);
     glPopMatrix();
 
@@ -44,16 +45,37 @@ void esfera()
 {
     glPushMatrix();
     glColor3d(1,0,0);
-    glTranslated(0,-1,-25);
+    glTranslated(camarax,0.55,camaraz-7);
     glRotated(0,0,0,0);
 
     glPushMatrix();
     glScaled(1,1,1);
-    glutSolidSphere(1,50,50);
+    glutSolidSphere(0.5,50,50);
     glPopMatrix();
 
 
     glPopMatrix();
+
+}
+
+void paredes()
+{
+    for(int i =-10;i>=-50;i-=10)
+    {
+        for(int j= -20;j<=25;j+=10)
+        {
+            glPushMatrix();
+            glTranslated(j,1.5,i);
+            glRotated(0,0,0,0);
+            glScaled(1,1,1);
+            glColor3d(0,1,0);
+            glutSolidCube(3);
+            glColor3d(0,0.8,0);
+            glutWireCube(3);
+            glPopMatrix();
+        }
+    }
+
 
 }
 
@@ -62,30 +84,36 @@ static void display(void)
     glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
     glMatrixMode(GL_MODELVIEW);
     glMatrixMode(GL_PROJECTION);
-    gluPerspective(45.0, ancho/alto, 0.1, 100.0);
-    gluLookAt(camarax,0,camaraz, camarax,0,camaraz-10,    0.0,    1,  0);
+    gluPerspective(50, ancho/alto, 0.1, 100.0);
+    gluLookAt(camarax,1,camaraz, camarax,0,camaraz-10,    0.0,    1,  0);
 
     esfera();
     piso();
+    paredes();
 
     glLoadIdentity();
     glFlush();
+}
+
+void teclasCamara(unsigned char key, int x, int y)
+{
+
 }
 
 void teclasEspeciales(int key, int x, int y)
 {
     switch (key) {
         case GLUT_KEY_UP :
-            camaraz-=0.1;
+            camaraz-=0.5;
             break;
         case GLUT_KEY_DOWN :
-            camaraz+=0.1;
+            camaraz+=0.5;
             break;
         case GLUT_KEY_LEFT :
-            camarax-=0.1;
+            camarax-=0.5;
             break;
         case GLUT_KEY_RIGHT :
-            camarax+=0.1;
+            camarax+=0.5;
             break;
     }
     display();
@@ -103,6 +131,7 @@ int main(int argc, char *argv[])
     glClearColor(0,0.7,1,0);
     glutDisplayFunc(display);
     glutSpecialFunc(teclasEspeciales);
+    glutKeyboardFunc(teclasCamara);
 
     glEnable(GL_DEPTH_TEST);
 
